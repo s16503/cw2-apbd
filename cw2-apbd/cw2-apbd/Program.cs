dfc0451bd05ac59b8275d0f42720022ad7d6b5fb
s16503 <s16503@pjwstk.edu.pl>
2020-03-09 16:22:13 +0100

    save1

---
+++ cw2-apbd/cw2-apbd/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+
+namespace cw2_apbd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            Console.WriteLine("Hello World!");
+
+
+            string path = @"data\dane.csv";
+            //wczytywanie 
+            var fi = new FileInfo(path);
+            using (var stream = new StreamReader(fi.OpenRead()))    //blok using gdy ammy metode Dispose , zwalnianie zasobów
+            {
+
+                string line = null;
+
+                while ((line = stream.ReadLine()) != null)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+
+            //stream.Dispose();
+
+
+            //XML
+            // - komunikacja i przesylanie informacji miedzy problemami
+            // - 
+
+
+
+        }
+    }
+}
---
+++ cw2-apbd/cw2-apbd/models/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace cw2_apbd.models
+{
+    public class Student
+    {
+        // prop + tabx2
+        public string Email { get; set; }
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
+
+
+    }
+}
